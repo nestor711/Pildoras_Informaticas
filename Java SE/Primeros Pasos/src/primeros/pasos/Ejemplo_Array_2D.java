4843d4d81d2b08a4bd11bba04329a3aa8049fcfe
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-03-31 08:52:31 -0500

    Add files via upload

---
+++ Java SE/Primeros Pasos/src/primeros/pasos/Ejemplo_Array_2D.java
@@ -0,0 +1,44 @@
+package primeros.pasos;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public class Ejemplo_Array_2D {
+
+	public static void main(String[] args) {
+
+		double acumulado;
+		double interes = 0.10;
+
+		double saldo[][] = new double[6][5];
+
+		for (int i = 0; i < 6; i++) {
+
+			saldo[i][0] = 10000;
+			acumulado = 10000;
+
+			for (int j = 1; j < 5; j++) {
+
+				acumulado = acumulado + (acumulado * interes);
+				saldo[i][j] = acumulado;
+
+			}
+
+			interes = interes + 0.01;
+
+		}
+
+		for (int z = 0; z < 6; z++) {
+
+			System.out.println("");
+
+			for (int h = 0; h < 5; h++) {
+
+				System.out.printf("%1.2f", saldo[z][h]);
+
+				System.out.println(" ");
+			}
+		}
+	}
+}
