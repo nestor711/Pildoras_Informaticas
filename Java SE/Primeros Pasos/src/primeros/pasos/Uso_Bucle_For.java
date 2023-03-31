761972c29725111c0beaaa1ab4b598f1a19db7ca
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-03-30 22:37:02 -0500

    Add files via upload

---
+++ Java SE/Primeros Pasos/src/primeros/pasos/Uso_Bucle_For.java
@@ -0,0 +1,27 @@
+package primeros.pasos;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public class Uso_Bucle_For {
+
+	public static void main(String[] args) {
+
+		// Bucle o Ciclo For (Incrementando)
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("Néstor");
+		}
+	}
+
+	public static void main2(String[] args) {
+
+		// Bucle o Ciclo For (Decrementando)
+		for (int i = 10; i > 0; i--) {
+
+			System.out.println("Néstor");
+		}
+	}
+
+}
