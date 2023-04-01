3b0514e12b24f855619be527393bc66989ac9b89
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-04-01 16:00:10 -0500

    Add files via upload

---
+++ Java SE/Primeros Pasos/src/poo/Clase1.java
@@ -0,0 +1,17 @@
+package poo;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public class Clase1 {
+	
+	int mivar = 5;
+	
+	int mivar2 = 7;
+	
+	String mimetodo(){
+		return "El valor de mi var2 es: " + mivar2;
+	}
+
+}
---
+++ Java SE/Primeros Pasos/src/poo/Clase2.java
@@ -0,0 +1,19 @@
+package poo;
+
+// Importar paquetes
+import paquetepruebas.Clase3;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public class Clase2 {
+
+	public static void main(String[] args) {
+		
+		Clase1 miobj = new Clase1();
+		Clase3 miobj2 = new Clase3();
+
+	}
+
+}
