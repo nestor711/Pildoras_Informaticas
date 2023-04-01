0f7bb9663c1551df629367b95dd82f4e74f84ed8
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-04-01 12:15:19 -0500

    Update Uso_Empleado.java

--- Java SE/Primeros Pasos/src/poo/Uso_Empleado.java
+++ Java SE/Primeros Pasos/src/poo/Uso_Empleado.java
@@ -24,19 +24,24 @@ public class Uso_Empleado {
 		System.out.println("Nombre: " + empleado2.dameNombre() + " Sueldo: " + empleado2.dameSueldo()+ " Fecha de Alta: " + empleado2.dameFechaContrato());
 		System.out.println("Nombre: " + empleado3.dameNombre() + " Sueldo: " + empleado3.dameSueldo()+ " Fecha de Alta: " + empleado3.dameFechaContrato());
 		 */
-		Empleado[] misEmpleados = new Empleado[4];
+		Jefatura jefe_RRHH = new Jefatura("Néstor", 55000, 2006, 9, 25);
+		jefe_RRHH.estableceIncentivo(2570);
+
+		Empleado[] misEmpleados = new Empleado[6];
 
 		misEmpleados[0] = new Empleado("Néstor Gutierrez", 85000, 1990, 12, 17);
 		misEmpleados[1] = new Empleado("David Heredia", 95000, 1995, 06, 02);
 		misEmpleados[2] = new Empleado("Fernando Gomez", 105000, 2002, 03, 15);
 		misEmpleados[3] = new Empleado("Primitivo Heredia");
+		misEmpleados[4] = jefe_RRHH; // Polimorfismo. Principio de sustitución
+		misEmpleados[5] = new Jefatura("María", 95000, 1999, 5, 26);
 
 		// Bucle o Ciclo for
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 6; i++) {
 			misEmpleados[i].subeSueldo(5);
 		}
 
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 6; i++) {
 			System.out.println("Nombre: " + misEmpleados[i].dameNombre() + " Sueldo: " + misEmpleados[i].dameSueldo() + " Fecha de Alta: " + misEmpleados[i].dameFechaContrato());
 		}
 
