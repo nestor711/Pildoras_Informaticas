c6f4266be1a5dc514dc38a129fc472303a00a5f7
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-04-01 15:59:35 -0500

    Add files via upload

---
+++ Java SE/Primeros Pasos/src/paquetepruebas/Clase3.java
@@ -0,0 +1,12 @@
+package paquetepruebas;
+
+// Importar paquetes
+import poo.Clase1;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public class Clase3 extends Clase1 {
+
+}
