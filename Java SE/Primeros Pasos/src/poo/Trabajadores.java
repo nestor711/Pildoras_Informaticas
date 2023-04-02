f6a02aa6c0fd2d572de908a3ade069ab4343e744
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-04-02 13:37:06 -0500

    Add files via upload

---
+++ Java SE/Primeros Pasos/src/poo/Trabajadores.java
@@ -0,0 +1,13 @@
+package poo;
+
+/**
+ *
+ * @author Néstor David Heredia Gutierrez (2058558-2711)
+ */
+public interface Trabajadores {
+
+	double establece_bonus(double gratificacion);
+
+	double bonus_base = 1500;
+
+}
